19f990cc560c3ce4ca81841a421487a24f85d96f
Beaxx <bleaxxy@gmail.com>
2017-12-19 20:13:33 +0100

    Exporter Klasse mit Methoden

---
+++ src/Gruppe7/Exporter/Exporter.java
@@ -0,0 +1,24 @@
+package Gruppe7.Exporter;
+
+public class Exporter
+{
+    public Exporter()
+    {
+        //Constructor
+    }
+
+    public void ExportKinoProgramm()
+    {
+        //Code
+    }
+
+    public void ExportFinanzplan()
+    {
+        //Code
+    }
+
+    public void ExportSaalplan()
+    {
+        //Code
+    }
+}
