7c3279eab266a0a9e34b481895ef7e786a6f0b18
FabianUe <34254877+FabianUe@users.noreply.github.com>
2018-01-19 22:52:20 +0100

    Genre fertig.

--- src/Gruppe7/Data/Genre.java
+++ src/Gruppe7/Data/Genre.java
@@ -1,12 +1,29 @@
 package Gruppe7.Data;
 
-public class Genre
-{
     //Define enum
-    enum genre{}
+    public enum Genre {
 
-    public Genre()
-    {
-        //Constructor
-    }
-}
+        ACTION("Action"),
+        DOKUMENTATION("Dokumentation"),
+        DRAMA("Drama"),
+        HORROR("Horror"),
+        KOMOEDIE("Komödie"),
+        KRIMI("Krimi"),
+        SCIENCE_FICTION("Science Fiction"),
+        THRILLER("Thriller"),
+        ZEICHENTRICK("Zeichentrick");
+
+
+        private String genereName;
+        Genre(String genreName){
+            this.genereName = genreName;
+        }
+
+        public String getGenereName(){
+            return this.genereName;
+        }
+
+            }
+
+
+
