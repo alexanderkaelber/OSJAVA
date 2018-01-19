9f670fad84bb87992e87ace56c51078486e278c2
Beaxx <bleaxxy@gmail.com>
2018-01-19 23:29:53 +0100

    Kinofilm fertig
    (cherry picked from commit 5faad4d)

---
+++ src/Gruppe7/Data/WerbefilmVerwaltung.java
@@ -0,0 +1,18 @@
+package Gruppe7.Data;
+
+import java.util.ArrayList;
+
+public class WerbefilmVerwaltung
+{
+    private ArrayList<Werbefilm> werbefilme = new ArrayList<Werbefilm>();
+
+    public WerbefilmVerwaltung()
+    {
+        //Constructor
+    }
+
+    //Getter
+    public ArrayList<Werbefilm> getWerbefilme() {
+        return werbefilme;
+    }
+}
