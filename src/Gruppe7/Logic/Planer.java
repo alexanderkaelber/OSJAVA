db37f816b9f7fa1b6e9db240ae6fd01fa45b81fe
FabianUe <34254877+FabianUe@users.noreply.github.com>
2018-01-19 20:29:42 +0100

    Saalklasse-fertig

--- src/Gruppe7/Logic/Planer.java
+++ src/Gruppe7/Logic/Planer.java
@@ -1,6 +1,7 @@
 package Gruppe7.Logic;
 
 import java.util.ArrayList;
+import java.util.Random;
 import Gruppe7.Data.Saal;
 import Gruppe7.Data.Spielplan;
 import Gruppe7.Data.Vorstellung;
@@ -8,12 +9,24 @@ import Gruppe7.Data.Vorstellung;
 public class Planer
 {
     //Attribute needs some work obviously
-    private randomSpielplan[] Vorstellung[21][Saal.Length][4];
-    private improvedSpielplan[] Vorstellung[21][Saal.Length][4];
+    private ArrayList<Vorstellung> randomSpielplan = new ArrayList<Vorstellung>(
+
+            //Initialerstellung des Plans bei Konstruktorcall
+            // Vorstellung[21][Saal.Length][4];
+    );
+    private ArrayList<Vorstellung> improvedSpielplan = new ArrayList<Vorstellung>();
 
     public Planer()
     {
         //Constructor
+    }
+
+    private void fillPlan(){
+        for (int i = 0; i < 21; i++) {
+            Vorstellung newVorstellung = new Vorstellung();
+
+            if () //TODO: hier weiter
+        }
     }
 
     private Vorstellung createVorstellung()
