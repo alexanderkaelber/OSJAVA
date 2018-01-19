95c1479917e823f893a4d47cb9969e77f5a53b56
FabianUe <34254877+FabianUe@users.noreply.github.com>
2018-01-19 23:16:42 +0100

    Fsk fertig.

--- src/Gruppe7/Data/Fsk.java
+++ src/Gruppe7/Data/Fsk.java
@@ -1,12 +1,21 @@
 package Gruppe7.Data;
 
-public class Fsk
-{
-    //Define enum
-    enum FSK{}
+public enum Fsk {
 
-    public Fsk()
-    {
-        //Constructor
+    FSK_0("0"),
+    FSK_6("6"),
+    FSK_12("12"),
+    FSK_16("16"),
+    FSK_18("18");
+
+    private String fskName;
+    Fsk(String fskName){
+        this.fskName = fskName;
     }
+
+    public String getFskName(){
+        return this.fskName;
+    }
+
+
 }
