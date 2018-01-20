432b4d4af980d123f9fbcb978a54053054fe90af
Beaxx <bleaxxy@gmail.com>
2018-01-20 01:39:11 +0100

    Comit mit kram
    (cherry picked from commit a862a7c)

--- src/Gruppe7/Data/FilmVerwaltung.java
+++ src/Gruppe7/Data/FilmVerwaltung.java
@@ -2,12 +2,11 @@ package Gruppe7.Data;
 
 import java.util.ArrayList;
 
-// Auf die Verwaltungsklassen muss aus dem gesamten Kode zugegriffen werden
 public class FilmVerwaltung
 {
-    static private ArrayList<Kinofilm> filme = new ArrayList<>();
+    static ArrayList<Kinofilm> filme = new ArrayList<>();
 
-    // Getter
-    public static ArrayList<Kinofilm> getFilme() { return filme; }
-    public static int getSize() { return filme.size(); }
+    static  ArrayList<Kinofilm> getFilme() {
+        return filme;
+    }
 }
