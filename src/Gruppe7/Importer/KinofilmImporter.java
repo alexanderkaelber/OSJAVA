904d60b0f60aea7ed48b1a5c0549734b1f123705
FabianUe <34254877+FabianUe@users.noreply.github.com>
2018-01-20 17:08:32 +0100

    Update am KinofilmImporter

--- src/Gruppe7/Importer/KinofilmImporter.java
+++ src/Gruppe7/Importer/KinofilmImporter.java
@@ -9,7 +9,12 @@ public class KinofilmImporter extends Datei {
     private String importString;
     private String importKinofilmTitel;
     private String importKinofilmRegisseur;
+
+    private int importKinofilmFskInt;
     private Fsk importKinofilmFSK;
+
+
+    private String importKinofilmGemrsString;
     private ArrayList<Genre> importKinofilmGenres = new ArrayList<Genre>(); // importKinofilmGenres.add()
     private int importKinofilmMietpreis;
     private int importKinofilmBeliebtheit;
@@ -18,6 +23,8 @@ public class KinofilmImporter extends Datei {
     private String importKinofilmErscheinungsland;
     private int importKinofilmErscheinungsjahr;
     private boolean importThreeD;
+
+    private int minBeliebtheit;
 
 
     /**
@@ -37,16 +44,47 @@ public class KinofilmImporter extends Datei {
         while (importFileKinofilme.eof() == false) {
             importString = importFileKinofilme.readLine_FS();
             if (importString != null) {
-                System.out.println("Import String:" + importString + "\naus Importdatei " + in_name);
+                System.out.println("Import String: " + importString + "\naus Importdatei " + in_name);
 
-               /* String arrayWerbung[] = importString.split(";");
+               String arrayKinofilm[] = importString.split(";");
 
-                importKinofilmTitel = String.valueOf(arrayWerbung[0]);
-                importKinofilmRegisseur = Integer.valueOf(arrayWerbung[1]);
-                importKinofilmLaufzeit = Integer.valueOf(arrayWerbung[2]);
+                importKinofilmTitel = String.valueOf(arrayKinofilm[0]);
+                importKinofilmRegisseur = String.valueOf(arrayKinofilm[1]);
+
+                importKinofilmFskInt = Integer.valueOf(arrayKinofilm[2]);
+                    if(importKinofilmFskInt== 18){
+                        importKinofilmFSK = Fsk.FSK_18;
+                    }
+                    if(importKinofilmFskInt== 16){
+                        importKinofilmFSK = Fsk.FSK_16;
+                    }
+                    if(importKinofilmFskInt== 12){
+                        importKinofilmFSK = Fsk.FSK_12;
+                    }
+                    if(importKinofilmFskInt== 6){
+                        importKinofilmFSK = Fsk.FSK_6;
+                    }
+                    if(importKinofilmFskInt== 0){
+                        importKinofilmFSK = Fsk.FSK_0;
+                    }
+                    System.out.println("FSK Enum gesetzt: " +importKinofilmFSK);
+                }
 
 
-                WerbefilmVerwaltung.setWerbefilme(new Werbefilm(importKinofilmTitel, importKinofilmLaufzeit, importKinofilmRegisseur));*/
+
+
+
+                        //ArrayList<Genre> TODO weiteres Array auslesen.
+               // importKinofilmGemrsString
+
+                //
+
+
+
+                //importKinofilmLaufzeit = Integer.valueOf(arrayKinofilm[6]);
+
+
+               // WerbefilmVerwaltung.setWerbefilme(new Werbefilm(importKinofilmTitel, importKinofilmLaufzeit, importKinofilmRegisseur));
 
             }
 
@@ -54,5 +92,5 @@ public class KinofilmImporter extends Datei {
 
 
     }
-}
+
 
