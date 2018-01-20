3b9c30aa266388e71e7295b05da80bac7cdbf9f2
Beaxx <bleaxxy@gmail.com>
2018-01-20 01:39:11 +0100

    Comit mit kram
    (cherry picked from commit a862a7c)

--- src/Gruppe7/Data/SaalVerwaltung.java
+++ src/Gruppe7/Data/SaalVerwaltung.java
@@ -1,9 +1,15 @@
 package Gruppe7.Data;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+// Auf die Verwaltungsklassen muss aus dem gesamten Kode zugegriffen werden
 public class SaalVerwaltung
 {
-    public SaalVerwaltung()
-    {
-        //Constructor
+
+    static ArrayList<Saal> saele = new ArrayList<Saal>();
+
+    public static ArrayList<Saal> getSaele() {
+        return saele;
     }
 }
