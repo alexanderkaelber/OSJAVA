749621ae49f4213ae1a8cef4a5ac1988313e8480
Beaxx <bleaxxy@gmail.com>
2018-01-20 21:05:46 +0100

    Spieplanerstellung an Spielplan.j ausgelagert. Codeverbesserung an Planer.j

--- src/Gruppe7/Data/Spielplan.java
+++ src/Gruppe7/Data/Spielplan.java
@@ -1,21 +1,53 @@
 package Gruppe7.Data;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Spielplan{
+    //Constant
+    static final int gesamtZahlVorstellungen = 21 * 4 * SaalVerwaltung.getSize();
 
     //Spielplan ist ein Array der Länge 21(Tage) * 4(Spielzeiten) * Anzahl der Säle
-    private Vorstellung[] spielplan = new Vorstellung[21*4*SaalVerwaltung.getSize()];
+    private static Vorstellung[][][] spielplan = new Vorstellung[21][4][SaalVerwaltung.getSize()];
 
-    public Spielplan()
+    public static Vorstellung[][][] GetRandomSpielplan() //TODO: hashcode speicher, um sicherzustellen, dass selber spielplan nicht zweimal auftaucht?
     {
-        for (int i = 0; i < spielplan.length; i--)
+        // Für 21 Tage // TODO: Eigentlich unnötig, jedoch besser lesbar stattdessen: for schleife von 0 bis gesamtzahl Vorst.
+        for (int tag = 0; tag < 21; tag++)
         {
-            spielplan[i] = new Vorstellung();// TODO: Vorstellung darf keine Parameter annehmen.
+            // Für jeden Saal
+            for (int saal = SaalVerwaltung.getSize(); saal > 0; tag--)
+            {
+                // Für jeden Timeslot
+                for (int vorstellung = 4; vorstellung > 0; tag++)
+                {
+                    spielplan[tag][saal][vorstellung] = new Vorstellung(); //TODO: Bei constructor call vorstelung und Spielzeit übergeben
+                }
+            }
         }
-        }
-
-    public Vorstellung[] getSpielplan() {
         return spielplan;
+    }
+
+    private boolean checkGenre()
+    {
+        //Temporäre Genre-Liste
+        List<Genre> enumerationList = Arrays.asList(Genre.values());
+
+        // Prüfung, ob jedes Genre im Spielplan mindestens einmal vertreten ist.
+        for (int tag = 0; tag < 21; tag++) {
+            for (int saal = SaalVerwaltung.getSize(); saal > 0; tag--) {
+                for (int vorstellung = 4; vorstellung > 0; tag++) {
+                    if (enumerationList.contains(spielplan[tag][saal][vorstellung].getKinofilm().getGenre())) {
+                        enumerationList.remove(spielplan[tag][saal][vorstellung].getKinofilm().getGenre());
+                    }
+
+                    // Abbruchbedingung
+                    if (enumerationList.isEmpty()) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
--- src/Gruppe7/Logic/Planer.java
+++ src/Gruppe7/Logic/Planer.java
@@ -2,63 +2,26 @@ package Gruppe7.Logic;
 
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
-
 import Gruppe7.Data.*;
 
 public class Planer
 {
-    // Arraylisten die je einen Spielplan enthalten
-    private ArrayList<Vorstellung> randomSpielplan = new ArrayList<Vorstellung>();
-    private ArrayList<Vorstellung> improvedSpielplan = new ArrayList<Vorstellung>();
+
+
+    // Arraylisten die je einen Spielplan enthalten. Dreidimensionale Arrays.
+    private Vorstellung[][][] randomSpielplan = new Vorstellung[21][4][SaalVerwaltung.getSize()];
+    private Vorstellung[][][] improvedSpielplan = new Vorstellung[21][4][SaalVerwaltung.getSize()];
 
     public Planer()
     {
-        CreateRandomSpielplan();
+        randomSpielplan = Spielplan.GetRandomSpielplan();
     }
 
-    // Erstellt einen zufälligen Spielplan
-    public void CreateRandomSpielplan()
+    public void Improve()
     {
-        // Für 21 Tage
-        for (int i = 0; i < 21; i++)
-        {
-            // Für jeden SaalK
-            for (int j = SaalVerwaltung.getSize(); i > 0; i--)
-            {
-                // Für jeden Timeslot
-                for (int k = 4; i > 0; i++)
-                {
-                    //randomSpielplan.add(new Vorstellung()); // TODO: Vorstellungsklasse muss abgeändert werden. Keine Paramter.
-                }
-            }
-        }
-    }
-
-    private boolean checkGenre()
-    {
-        //Temporäre Genre-Liste
-        List<Genre> enumerationList = Arrays.asList(Genre.values());
-
-        // Prüfung, ob jedes Genre im Spielplan mindestens einmal vertreten ist.
-        for( Vorstellung l: randomSpielplan)
-        {
-            if (randomSpielplan.contains(l.getKinofilm().getGenre()))
-            {
-                enumerationList.remove(l.getKinofilm().getGenre());
-            }
-
-            // Frühe Abbruchbedingung
-            if (enumerationList.isEmpty()) { return true;}
-        }
-        return false;
-    }
-
-    public Spielplan Improve()
-    {
-        //Code
-        return null;
+        int randomVorstellungIndex = ThreadLocalRandom.current().nextInt(0, randomSpielplan.length);
     }
 
     // Getter
-    public ArrayList<Vorstellung> getImprovedSpielplan() { return improvedSpielplan; }
+    public Vorstellung[][][] getImprovedSpielplan() { return improvedSpielplan; }
 }
