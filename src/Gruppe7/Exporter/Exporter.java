2c3914f6087790f52ce36c284c9885b6b13ebb04
Nicole Distler <nicoledistler@t-online.de>
2018-01-21 02:22:27 +0100

    exporter

--- src/Gruppe7/Exporter/Exporter.java
+++ src/Gruppe7/Exporter/Exporter.java
@@ -1,11 +1,30 @@
 package Gruppe7.Exporter;
 
-public class Exporter
-{
-    public Exporter()
-    {
-        //Constructor
+import Gruppe7.Data.Spielplan;
+import sun.security.provider.ConfigFile;
+
+import java.io.*;
+
+public class Exporter {
+
+    private Spielplan[] exportSpielplan;
+    String[] spielplan = Spielplan.(",");
+
+
+
+    public Exporter(){
+
+        getExportSpielplan();
     }
+
+    public Spielplan[] getExportSpielplan() {
+        return exportSpielplan; }
+
+
+}
+
+
+
 
     public void ExportKinoProgramm()
     {
@@ -21,4 +40,12 @@ public class Exporter
     {
         //Code
     }
+
 }
+
+/*Spielplan = Array aus Vorstellungen
+muss diesen wieder auseinandernehmen
+alles erstmal über System.out.println() für drei Exporte
+aus Spielplan die Vorstellung über das Array nehmen [0] etc., dann über die Objekte film, etc. die einzelnen
+Attribute herausnehmen und dann in Tabellen einbauen
+ */
