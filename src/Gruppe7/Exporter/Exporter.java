81a8d5ea5b72b4f7d4e5efb94bc1ae991ad60499
FabianUe <34254877+FabianUe@users.noreply.github.com>
2018-01-21 13:35:29 +0100

    Kinofil -> Genre Arraylist hinzugefügt
    (cherry picked from commit 828efd1)

--- src/Gruppe7/Exporter/Exporter.java
+++ src/Gruppe7/Exporter/Exporter.java
@@ -7,12 +7,27 @@ public class Exporter
 {
 
     private Vorstellung[][][] exportSpielplan;
+    private int vorstellungsTagNr;
+
 
     public Exporter(Vorstellung[][][] in_exportSpielplan)
     {
         exportSpielplan=in_exportSpielplan;
+                                //[TAG][SAAL][TIMESLOT]
+//        System.out.println(exportSpielplan[0][0][0]);
 
-        System.out.println(exportSpielplan[0][0][0]);
+        // for (int i = 0; i < arrayGenre.length; i++) {
+        System.out.println(exportSpielplan.length);
+        for (int i=0; i<= 20; i++){
+
+            System.out.println(i);
+
+            System.out.println(exportSpielplan[i][0][0]);
+        }
+
+        //vorstellungsTagNr=exportSpielplan[][][];
+
+
 
         //Constructor [TAG][SAAL][TIMESLOT]
 
